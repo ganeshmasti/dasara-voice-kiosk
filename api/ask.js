105d3513abc6b66f74efc180ffcc08c4c7f62163
Ganesh Gopal Masti Jayaram <ganeshmj@gmail.com>
2025-09-21 08:10:26 -0700

    Create ask.js

---
+++ api/ask.js
@@ -0,0 +1,11 @@
+# in a terminal
+mkdir dasara-voice-kiosk && cd dasara-voice-kiosk
+mkdir api
+# create files with your editor, paste the contents below
+git init
+git add .
+git commit -m "init"
+# push to GitHub (replace USERNAME and created repo URL)
+git branch -M main
+git remote add origin https://github.com/USERNAME/dasara-voice-kiosk.git
+git push -u origin main
